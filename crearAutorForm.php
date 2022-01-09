59e3f5ffb682bdb2ae85f216bc70cc8a4829a5c2
Maxi <96454261+Maxim240@users.noreply.github.com>
2022-01-09 20:52:57 -0300

    Add files via upload

--- crearAutorForm.php
+++ crearAutorForm.php
@@ -41,6 +41,17 @@ if(isset($_SESSION["nombre"])){
           <input type="text" class="form-control" name="apAutor"/>
           <input type="submit" class="form-control" name="enviar" value="Enviar datos">
 </form>
+<!-- <?php 
+              // include_once('conexion.php'); -->
+              // $con=conectar();
+              // $query="SELECT*FROM autor";
+              // $resultado=$con->query($query);
+              // while ($datos=$resultado->fetch_assoc()){
+              // ?>
+              // <option value="<?php echo $datos["idAutor"];?>"> <?php echo $datos[ "nombre"]."".$datos["apellido"]; ?></option>     
+              //   <?php
+              // }
+              // ?>s
     <!-- Popper and Boostrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
